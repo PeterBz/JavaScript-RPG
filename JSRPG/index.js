0a2dc40dd13a47c9a03fd1e6a493def8a1d9aed0
Peter <peter_brunzel@web.de>
2026-02-13 18:12:59 +0100

    index.html set up, locations added

---
+++ JSRPG/index.js
@@ -0,0 +1,96 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterhealth;
+let inventory = ["stick"];
+
+// alle Elemente aus dem html als const deklarieren damit wir sie updaten können
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+
+const monsterName = document.querySelector("#monsterName");
+const monsterHealth = document.querySelector("#monsterHealth");
+const monsterStats = document.querySelector("#monsterStats");
+
+const xpText = document.querySelector("#xpText");
+const goldText = document.querySelector("#goldText");
+const healthText = document.querySelector("#healthText");
+
+// locations als array speichern
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button function": [goStore, goCave, fightDragon],
+    text: ['You are in the town. You see a sign that says "store".'],
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button function": [buyHealth, buyWeapon, goTown],
+    text: ["You are in the store."],
+  },
+   {
+    name: "cave",
+    "button text": [
+      "figh slime",
+      "figh beast",
+      "Go to town square",
+    ],
+    "button function": [buyHealth, buyWeapon, goTown],
+    text: ["You enter the cave."],
+  }
+];
+
+// Button initialisieren onClick
+button1.addEventListener("click", goStore);
+button2.addEventListener("click", goCave);
+button3.addEventListener("click", fightDragon);
+
+function update(location) {
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.addEventListener("click", location["button function"][0]);
+  button2.addEventListener("click", location["button function"][1]);
+  button3.addEventListener("click", location["button function"][2]);
+  text.innerText = location.text;
+
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+
+  button1.addEventListener("click", location["button function"][0]);
+  button2.addEventListener("click", location["button function"][1]);
+  button3.addEventListener("click", location["button function"][2]);
+  text.innerText = "You enter the store";
+}
+
+function goTown() {
+  //update funnction aufrufen
+  update(locations[0]);
+}
+
+function goStore() {
+  update(locations[1]);
+}
+
+function goCave() {
+  update(locations[2]);
+}
+
+function fightDragon() {
+  console.log("Going to fight the Dragon.");
+}
+
+function buyHealth() {}
+function buyWeapon() {}
